f7cd6a1104215d1800ab1070b4d6d8671b67a5d7
yoonsang lee <highp0912@pm.me>
2022-06-13 23:07:44 +0900

    fix: 경로 에러

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -1,8 +1,8 @@
-import { RecipeView } from './recipeView.js';
-import { SearchView } from './searchView.js';
-import { ResultsView } from './resultsView.js';
+import { RecipeView } from './view.impl/recipeView.js';
+import { SearchView } from './view.impl/searchView.js';
+import { ResultsView } from './view.impl/resultsView.js';
 import { PaginationView } from './view.impl/paginationView.js';
-import { BookmarksView } from './bookmarksView.js';
+import { BookmarksView } from './view.impl/bookmarksView.js';
 import { AddRecipeView } from './view.impl/addRecipeView.js';
 
 export {
